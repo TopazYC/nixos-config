a0d2b6faa68fbfebb67d916eab8797fbb262cd46
TopazYC <ycroaker@gmail.com>
2026-09-22 16:32:57 +0800

    fisher minor

--- fisher/proxy.sh
+++ fisher/proxy.sh
@@ -1,8 +1,8 @@
 sudo mkdir -p /run/systemd/system/nix-daemon.service.d/
 sudo tee /run/systemd/system/nix-daemon.service.d/override.conf <<EOF
 [Service]
-Environment="https_proxy=https://YCROAKER-LAPTOP:7890"
-Environment="http_proxy=http://YCROAKER-LAPTOP:7890"
+Environment="https_proxy=https://127.0.0.1:7897"
+Environment="http_proxy=http://127.0.0.1:7897"
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart nix-daemon
