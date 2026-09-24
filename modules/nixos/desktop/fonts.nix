b1f884af4408cca6eb62713f7c39e36c9a946622
TopazYC <ycroaker@gmail.com>
2026-09-24 15:52:28 +0800

    fix kmscon

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -54,12 +54,30 @@
     # kmscon is a kms/dri-based userspace virtual terminal implementation.
     # It supports a richer feature set than the standard linux console VT,
     # including full unicode support, and when the video card supports drm should be much faster.
+
+# the following configuration only supported in master branch.
+#    enable = true;
+#    extraOptions = "--term xterm-256color";
+#    config = {
+#      font-name = "Maple Mono NF CN";
+#      font-size = 14;
+#      hwaccel = true;
+#    };
+
     enable = true;
+    fonts = with pkgs; [
+      {
+        name = "Maple Mono NF CN";
+        package = maple-mono.NF-CN-unhinted;
+      }
+      {
+        name = "JetBrainsMono Nerd Font";
+        package = nerd-fonts.jetbrains-mono;
+      }
+    ];
     extraOptions = "--term xterm-256color";
-    config = {
-      font-name = "Maple Mono NF CN";
-      font-size = 14;
-      hwaccel = true;
-    };
   };
+    extraConfig = "font-size=14";
+    # Whether to use 3D hardware acceleration to render the console.
+    hwRender = true;
 }
